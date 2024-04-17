373533fe4fb3b3038be4a9b4640c2699ffd364a4
Cristian Perez <cperez@labelgrup.com>
2024-04-17 11:13:05 +0200

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
-    <div className="max-w-[50%] m-auto">
+    <div className="lg:w-[50%] w-[80%] m-auto py-10">
       <h1 className="font-bold text-center text-2xl py-5"> Doc. Library React Charts</h1>
       <ChartsView />
     </div>
