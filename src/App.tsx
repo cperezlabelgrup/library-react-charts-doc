96105c0e278cf23d48e29dc2eb727fab1ff12668
Cristian Perez <cperez@labelgrup.com>
2024-04-17 11:09:37 +0200

    charts examples

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ChartsView from "./components/ChartsView"
+
+
+function App() {
+
+
+  return (
+    <div className="max-w-[50%] m-auto">
+      <h1 className="font-bold text-center text-2xl py-5"> Doc. Library React Charts</h1>
+      <ChartsView />
+    </div>
+  )
+}
+
+export default App
